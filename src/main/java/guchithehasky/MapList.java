f36790c5a232b78149577849c706d1caeb893318
GuchiTheHasky <guchithehasky@gmail.com>
2023-01-28 15:57:05 +0200

    Add comment

--- src/main/java/guchithehasky/MapList.java
+++ src/main/java/guchithehasky/MapList.java
@@ -1,6 +1,6 @@
 package guchithehasky;
 
-public interface MapList<K, V> extends Iterable<V> {
+public interface MapList<K, V> extends Iterable<V> { //comment
     boolean insert (K key, V value);
     boolean delete (K key);
     V get(K key);
